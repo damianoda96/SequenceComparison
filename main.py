cbda7a952e8f6b365f8536095e85436f1e5c8ccb
Deven Damiano <damiano.da96@gmail.com>
2019-02-27 10:33:39 -0500

    creating value table

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
-def align(s, t):
+def make_table(s, t):
 	# local alignment Smith and Waterman
 
-	print(s)
-	print(t)
-
-	vals_x = []
+	vals = []
 
 	match = 1
 	mismatch = -1
 	gap = -2
 
-	for i in range(len(t)):
+	for i in range(len(t) + 1): # 1 extra for empty row / column
 
 		vals_y = []
 
-		for j in range(len(s)):
+		for j in range(len(s) + 1): # 1 extra for empty row / column
 			
 			vals_y.append(0)
 
-		vals_x.append(vals_y)
+		vals.append(vals_y)
 		del vals_y
 
-			# do something here...
+	# print(s)
+	for i in range(len(t) + 1):
+		'''if i > 0:
+			print(t[i - 1] + " "  + str(vals_x[i]))
+		else:
+			print("  "  + str(vals_x[i]))'''
 
-	print(s)
-	for i in range(len(t)):
-		print(t[i] + " "  + str(vals_x[i]))
+		print(vals[i])
+
+
+
+def align(table, s, t):
+	pass
+
 
 def main():
 
 	s = "GATCACCT"
 	t = "GATACCC"
 
-	align(s, t)
+	table = make_table(s, t)
+
+	align(table, s, t)
 
 main()
