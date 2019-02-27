63ff9ff8277dd08b0abd9342e24ac7615a1e34b2
Deven Damiano <damiano.da96@gmail.com>
2019-02-27 10:40:24 -0500

    blah

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ def make_table(s, t):
 		del vals_y
 
 	# print(s)
+
+	print("\n")
+
 	for i in range(len(t) + 1):
 		'''if i > 0:
 			print(t[i - 1] + " "  + str(vals_x[i]))
@@ -26,11 +29,26 @@ def make_table(s, t):
 			print("  "  + str(vals_x[i]))'''
 
 		print(vals[i])
+		
+
+	print("\n#####################################################\n")
+
+	return vals
 
 
+def align(table, s, t): # utilize Smith and Wunsch algorithm
+	
+	for i in range(len(table)):
+		for j in range(len(table[i])):
+			if i == 0 or j == 0:
+				table[i][j] = 1
 
-def align(table, s, t):
-	pass
+	# print out to verify
+
+	for i in range(len(table)):	
+
+		print(table[i])
+
 
 
 def main():
@@ -41,5 +59,7 @@ def main():
 	table = make_table(s, t)
 
 	align(table, s, t)
+
+	# print("\n")
 
 main()
